bda42e0d76a0ad24ca077d9a03d21f09edbcb6fa
Peter Williams <peter@newton.cx>
2023-03-23 18:31:15 -0400

    util.ts: reformat through VS Code

--- util.ts
+++ util.ts
@@ -14,11 +14,11 @@ export async function parseXmlFromUrl(url: string): Promise<Document> {
 }
 
 export function snakeToPascal(str: string) {
-return str.split("/")
-  .map(snake => snake.split("_")
-  .map(substr => substr.charAt(0)
-      .toUpperCase() +
-      substr.slice(1))
-  .join(""))
-  .join("/");
+  return str.split("/")
+    .map(snake => snake.split("_")
+      .map(substr => substr.charAt(0)
+        .toUpperCase() +
+        substr.slice(1))
+      .join(""))
+    .join("/");
 };
