b4365d5616712186de5d14c3ae68221fc2f4ed82
Carifio24 <carifio24@gmail.com>
2023-12-20 11:08:55 -0500

    Modify to acount that a day can have multiple features.

--- features.ts
+++ features.ts
@@ -3,10 +3,10 @@ import { isLeft } from "fp-ts/lib/Either.js";
 import { PathReporter } from "io-ts/lib/PathReporter.js";
 import { Response } from "express";
 import { Request as JwtRequest } from "express-jwt";
-import { ObjectId, WithId } from "mongodb";
+import { FindCursor, ObjectId, WithId } from "mongodb";
 
 import { State } from "./globals.js";
-import { sceneToJson, sceneToPlace } from "./scenes.js";
+import { sceneToJson } from "./scenes.js";
 
 export interface MongoSceneFeature {
   scene_id: ObjectId; 
@@ -29,13 +29,12 @@ function timeStrippedDate(date: Date): Date {
   return day;
 }
 
-export async function getFeatureForDate(state: State, date: Date): Promise<WithId<MongoSceneFeature> | null> {
+export async function getFeaturesForDate(state: State, date: Date): Promise<FindCursor<WithId<MongoSceneFeature>>> {
   const day = timeStrippedDate(date);
   const nextDay = new Date(day);
   nextDay.setDate(nextDay.getDate() + 1);
 
-  // We're assuming that there's only one feature!
-  return state.features.findOne({
+  return state.features.find({
     "$and": [
       { feature_time: { "$gte": day } },
       { feature_time: { "$lt": nextDay } } 
@@ -93,9 +92,18 @@ export function initializeFeatureEndpoints(state: State) {
 
       const input: FeatureCreationT = maybe.right;
 
+      const date = new Date(input.feature_time);
+      if (isNaN(date.getTime())) {
+        res.status(400).json({
+          error: true,
+          message: "Invalid date specified",
+        });
+        return;
+      }
+
       const record: MongoSceneFeature = {
         scene_id: new ObjectId(input.scene_id),
-        feature_time: new Date(input.feature_time),
+        feature_time: date,
       };
 
       try {
@@ -115,232 +123,269 @@ export function initializeFeatureEndpoints(state: State) {
 
 
   state.app.get(
-    "/feature/:date",
+    "/features/:date",
     async (req: JwtRequest, res: Response) => {
       const date = new Date(req.params.date);
       if (isNaN(date.getTime())) {
         res.status(400).json({
           error: true,
-          message: "Invalid date specified"
+          message: "Invalid date specified",
         });
         return;
       }
 
-      const feature = await getFeatureForDate(state, date);
-      if (feature === null) {
-        res.status(404).json({
-          error: true,
-          message: "No feature found for given date"
-        });
-        return;
+      const features = await getFeaturesForDate(state, date);
+      const hydratedFeatures: HydratedSceneFeature[] = [];
+      for await (const feature of features) {
+        const hydrated = await hydratedFeature(state, feature, req);
+        hydratedFeatures.push(hydrated);
       }
 
-      const hydrated = await hydratedFeature(state, feature, req);
-      
       res.json({
         error: false,
-        feature: hydrated 
+        features: hydratedFeatures
       });
 
     });
 
-    state.app.get(
-      "/features",
-      async (req: JwtRequest, res: Response) => {
-        const startDate = new Date(Number(req.query.start_date));
-        const endDate = new Date(Number(req.query.end_date));
+  state.app.get(
+    "/features",
+    async (req: JwtRequest, res: Response) => {
+      const startDate = new Date(Number(req.query.start_date));
+      const endDate = new Date(Number(req.query.end_date));
 
-        if (isNaN(startDate.getTime()) || isNaN(endDate.getTime())) {
-          res.status(400).json({
-            error: true,
-            message: "Invalid start and end date formats"
-          });
-          return;
-        }
-
-        const features = await getFeaturesForRange(state, startDate, endDate);
-        const hydratedFeatures: HydratedSceneFeature[] = [];
-        for await (const feature of features) {
-          const hydrated = await hydratedFeature(state, feature, req);
-          hydratedFeatures.push(hydrated);
-        }
-
-        res.json({
-          error: false,
-          features: hydratedFeatures
+      if (isNaN(startDate.getTime()) || isNaN(endDate.getTime())) {
+        res.status(400).json({
+          error: true,
+          message: "Invalid start and end date formats"
         });
+        return;
+      }
+
+      const features = await getFeaturesForRange(state, startDate, endDate);
+      const hydratedFeatures: HydratedSceneFeature[] = [];
+      for await (const feature of features) {
+        const hydrated = await hydratedFeature(state, feature, req);
+        hydratedFeatures.push(hydrated);
+      }
+
+      res.json({
+        error: false,
+        features: hydratedFeatures
+      });
+    });
+
+  state.app.get(
+    "/features/queue",
+    async (req: JwtRequest, res: Response) => {
+      const queueDoc = await state.featureQueue.findOne();
+      const sceneIDs = queueDoc?.scene_ids ?? [];
+      if (sceneIDs.length === 0) {
+        res.status(500).json({
+          error: true,
+          message: "No scene present in queue",
+        });
+        return;
+      }
+
+      const scenes: Record<string, any>[] = [];
+      for (const id of sceneIDs) {
+        const scene = await state.scenes.findOne({ "_id": new ObjectId(id) });
+        if (scene === null) {
+          throw new Error(`Database consistency failure, feature queue missing scene ${id}`);
+        }
+        const sceneJson = await sceneToJson(scene, state, req.session);
+        scenes.push(sceneJson);
+      }
+
+      res.json({
+        error: false,
+        scenes
       });
 
-      state.app.get(
-        "/features/queue",
-        async (req: JwtRequest, res: Response) => {
-          const queueDoc = await state.featureQueue.findOne();
-          const sceneIDs = queueDoc?.scene_ids ?? [];
-          if (sceneIDs.length === 0) {
-            res.status(500).json({
-              error: true,
-              message: "No scene present in queue",
-            });
-            return;
-          }
+    });
 
-          const scenes: Record<string, any>[] = [];
-          for (const id of sceneIDs) {
-            const scene = await state.scenes.findOne({ "_id": new ObjectId(id) });
-            if (scene === null) {
-              throw new Error(`Database consistency failure, feature queue missing scene ${id}`);
-            }
-            const sceneJson = await sceneToJson(scene, state, req.session);
-            scenes.push(sceneJson);
-          }
-
-          res.json({
-            error: false,
-            scenes
-          });
-
+  state.app.post(
+    "/features/queue",
+    async (req: JwtRequest, res: Response) => {
+      const ids = req.body.scene_ids;
+      const valid = Array.isArray(ids) && ids.every(id => typeof id === "string");
+      if (!valid) {
+        res.status(400).json({
+          error: true,
+          message: "Scene IDs should be an array of strings",
         });
+        return;
+      }
 
-      state.app.get(
-        "/features/queue/next",
-        async (req: JwtRequest, res: Response) => {
-          const queueDoc = await state.featureQueue.findOne({ queue: true });
-          const sceneIDs = queueDoc?.scene_ids ?? [];
-          if (sceneIDs.length === 0) {
-            res.status(500).json({
-              error: true,
-              message: "No scenes present in queue",
-            });
-            return;
-          }
-          const id = sceneIDs[0];
-          
-          const scene = await state.scenes.findOne({ "_id": new ObjectId(id) });
-          if (scene === null) {
-            throw new Error(`Database consistency failure, feature queue missing scene ${id}`);
-          }
-          const sceneJson = await sceneToJson(scene, state, req.session);
-
-          res.json({
-            error: false,
-            scene: sceneJson,
-          });
-
+      const objectIDs = ids.map((id: string) => new ObjectId(id));
+      const scenes = await state.scenes.find({ "_id": { "$in" : objectIDs } }).toArray();
+      if (scenes.length !== objectIDs.length) {
+        res.status(400).json({
+          error: true,
+          message: "At least one of the scene IDs does not correspond to a valid scene",
         });
+        return;
+      }
 
-      state.app.post(
-        "/features/pop",
-        async (req: JwtRequest, res: Response) => {
-          const result = await state.featureQueue.findOneAndUpdate(
-            { queue: true },
-            { "$pop": { "scene_ids": 1 } }  // -1 pops the first element: https://www.mongodb.com/docs/manual/reference/operator/update/pop/
-          );
+      const result = await state.featureQueue.updateOne(
+        { queue: true },
+        { "$set": { scene_ids: objectIDs } },
+      );
 
-          const queueDoc = result.value;
-          if (queueDoc === null) {
-            throw new Error(`Database failure, queue is missing`);
-          }
-
-          const id = queueDoc.scene_ids[0];
-          const scene = await state.scenes.findOne({ "_id": new ObjectId(id) });
-          if (scene === null) {
-            throw new Error(`Database consistency failure, feature queue missing scene ${id}`);
-          }
-          const sceneJson = await sceneToJson(scene, state, req.session);
-
-          res.json({
-            error: false,
-            scene: sceneJson
-          });
+      if (result.modifiedCount === 1) {
+        res.json({
+          error: false,
+          message: "Queue updated successfully"
         });
-
-      state.app.post(
-        "/features/queue/:id",
-        async (req: JwtRequest, res: Response) => {
-          const id = req.params.id;
-          const objectId = new ObjectId(id);
-          const scene = await state.scenes.findOne({ "_id": objectId });
-
-          if (scene === null) {
-            res.status(400).json({
-              error: true,
-              message: `Invalid scene id ${id}`
-            });
-            return;
-          }
-
-          const queueDoc = await state.featureQueue.findOne({ queue: true });
-          if (queueDoc === null) {
-            throw new Error(`Database failure, queue is missing`);
-          }
-
-          const inQueue = queueDoc.scene_ids.some(oid => oid.equals(objectId));
-          if (inQueue) {
-            res.json({
-              error: false,
-              message: `Scene ${id} is already in the queue`
-            });
-            return;
-          }
-
-          const updateResult = await state.featureQueue.updateOne(
-            { queue: true },
-            { "$push": { scene_ids: objectId } }
-          );
-
-          if (updateResult.modifiedCount === 0) {
-            res.status(500).json({
-              error: true,
-              message: `Error adding ${id} to queue`,
-            });
-          } else {
-            res.json({
-              error: false,
-              message: `Scene ${id} added to queue`,
-            });
-          }
-
+      } else {
+        res.status(500).json({
+          error: true,
+          message: "Error updating queue in database"
         });
+      }
+    });
 
-      state.app.delete(
-        "/features/queue/:id",
-        async (req: JwtRequest, res: Response) => {
-          const id = req.params.id;
-          const objectId = new ObjectId(id);
-          const queueDoc = await state.featureQueue.findOne({ queue: true });
-          if (queueDoc === null) {
-            throw new Error(`Database failure, queue is missing`);
-          }
-
-          const inQueue = queueDoc.scene_ids.some(oid => oid.equals(objectId));
-
-          if (!inQueue) {
-            res.json({
-              error: false,
-              message: `Scene ${id} was not in queue`,
-            });
-            return;
-          }
-
-          const newQueue = queueDoc.scene_ids;
-          newQueue.push(objectId);
-          const updateResult = await state.featureQueue.updateOne(
-            { queue: true },
-            { scene_ids: newQueue }
-          );
-          
-          if (updateResult.modifiedCount === 0) {
-            res.status(500).json({
-              error: true,
-              message: `Error removing ${id} from queue`,
-            });
-          } else {
-            res.json({
-              error: false,
-              message: `Scene ${id} removed from queue`,
-            });
-          }
-
+  state.app.get(
+    "/features/queue/next",
+    async (req: JwtRequest, res: Response) => {
+      const queueDoc = await state.featureQueue.findOne({ queue: true });
+      const sceneIDs = queueDoc?.scene_ids ?? [];
+      if (sceneIDs.length === 0) {
+        res.status(500).json({
+          error: true,
+          message: "No scenes present in queue",
         });
+        return;
+      }
+      const id = sceneIDs[0];
+      
+      const scene = await state.scenes.findOne({ "_id": new ObjectId(id) });
+      if (scene === null) {
+        throw new Error(`Database consistency failure, feature queue missing scene ${id}`);
+      }
+      const sceneJson = await sceneToJson(scene, state, req.session);
+
+      res.json({
+        error: false,
+        scene: sceneJson,
+      });
+
+    });
+
+  state.app.post(
+    "/features/pop",
+    async (req: JwtRequest, res: Response) => {
+      const result = await state.featureQueue.findOneAndUpdate(
+        { queue: true },
+        { "$pop": { "scene_ids": 1 } }  // -1 pops the first element: https://www.mongodb.com/docs/manual/reference/operator/update/pop/
+      );
+
+      const queueDoc = result.value;
+      if (queueDoc === null) {
+        throw new Error(`Database failure, queue is missing`);
+      }
+
+      const id = queueDoc.scene_ids[0];
+      const scene = await state.scenes.findOne({ "_id": new ObjectId(id) });
+      if (scene === null) {
+        throw new Error(`Database consistency failure, feature queue missing scene ${id}`);
+      }
+      const sceneJson = await sceneToJson(scene, state, req.session);
+
+      res.json({
+        error: false,
+        scene: sceneJson
+      });
+    });
+
+  state.app.post(
+    "/features/queue/:id",
+    async (req: JwtRequest, res: Response) => {
+      const id = req.params.id;
+      const objectId = new ObjectId(id);
+      const scene = await state.scenes.findOne({ "_id": objectId });
+
+      if (scene === null) {
+        res.status(400).json({
+          error: true,
+          message: `Invalid scene id ${id}`
+        });
+        return;
+      }
+
+      const queueDoc = await state.featureQueue.findOne({ queue: true });
+      if (queueDoc === null) {
+        throw new Error(`Database failure, queue is missing`);
+      }
+
+      const inQueue = queueDoc.scene_ids.some(oid => oid.equals(objectId));
+      if (inQueue) {
+        res.json({
+          error: false,
+          message: `Scene ${id} is already in the queue`
+        });
+        return;
+      }
+
+      const updateResult = await state.featureQueue.updateOne(
+        { queue: true },
+        { "$push": { scene_ids: objectId } }
+      );
+
+      if (updateResult.modifiedCount === 0) {
+        res.status(500).json({
+          error: true,
+          message: `Error adding ${id} to queue`,
+        });
+      } else {
+        res.json({
+          error: false,
+          message: `Scene ${id} added to queue`,
+        });
+      }
+
+    });
+
+  state.app.delete(
+    "/features/queue/:id",
+    async (req: JwtRequest, res: Response) => {
+      const id = req.params.id;
+      const objectId = new ObjectId(id);
+      const queueDoc = await state.featureQueue.findOne({ queue: true });
+      if (queueDoc === null) {
+        throw new Error(`Database failure, queue is missing`);
+      }
+
+      const inQueue = queueDoc.scene_ids.some(oid => oid.equals(objectId));
+
+      if (!inQueue) {
+        res.json({
+          error: false,
+          message: `Scene ${id} was not in queue`,
+        });
+        return;
+      }
+
+      const newQueue = queueDoc.scene_ids;
+      newQueue.push(objectId);
+      const updateResult = await state.featureQueue.updateOne(
+        { queue: true },
+        { scene_ids: newQueue }
+      );
+      
+      if (updateResult.modifiedCount === 0) {
+        res.status(500).json({
+          error: true,
+          message: `Error removing ${id} from queue`,
+        });
+      } else {
+        res.json({
+          error: false,
+          message: `Scene ${id} removed from queue`,
+        });
+      }
+
+    });
 
 }
